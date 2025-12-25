7f0d20ee9f722041d6f6e4e6de5fd3eeb1f6efc1
Zohaib <zohaibv1999@gmail.com>
2025-12-25 01:04:43 -0500

    load faster. locked scroll till loaded

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { useEffect, useRef, useState } from "react";
 
 function App() {
 
+  
   const useTimeSinceLoad = () => {
     const startTime = useRef(performance.now());
     const [elapsed, setElapsed] = useState(0);
@@ -31,7 +32,22 @@ function App() {
     return elapsed;
   };
 
+  
+
   const elapsed = useTimeSinceLoad();
+
+  useEffect(() => {
+    if (elapsed < 2500) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [elapsed]);
+
 
   return (
     <>
@@ -47,11 +63,11 @@ function App() {
 
       <div
           className={`fixed inset-0 z-[40] bg-white transition-opacity duration-700
-          ${elapsed > 2700 ? "opacity-0 pointer-events-none" : "opacity-100"}`}
+          ${elapsed > 2000 ? "opacity-0 pointer-events-none" : "opacity-100"}`}
       />
 
       
-      {elapsed > 1200 && <Banner />} 
+      {elapsed > 1000 && <Banner />} 
       <Projects />
       <Experience />
       <Skills />
